92000f707c452a08b0978603e0af83095fc293a1
RomanErt <roovolution@gmail.com>
2024-07-27 18:34:49 -0700

    Homework for lesson 3 (4 tasks)

--- homework/roman_b/Lesson_3/task_2.py
+++ homework/roman_b/Lesson_3/task_2.py
@@ -6,8 +6,10 @@ first_number = 10
 second_number = 17
 
 my_calculation = (
-    first_number - second_number
-    + first_number * second_number
-)
+    first_number
+    - second_number
+    + first_number
+    * second_number
+    )
 
 print(f'Your result is: {my_calculation}')
